291c20c9d8966c433d33777c4f0809af2716638a
fjalvarez84 <13156694@alu.murciaeduca.es>
2024-03-03 13:08:17 +0100

    Update TareaGitEntornosDesarrollo.java

--- Tarea-git-entornos-desarrollo/src/main/java/com/mycompany/tarea/git/entornos/desarrollo/TareaGitEntornosDesarrollo.java
+++ Tarea-git-entornos-desarrollo/src/main/java/com/mycompany/tarea/git/entornos/desarrollo/TareaGitEntornosDesarrollo.java
@@ -14,5 +14,6 @@ public class TareaGitEntornosDesarrollo {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Modificado en Net Beans");
+        System.out.println("Modificado en Github");
     }
 }
